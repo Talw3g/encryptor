a8ca4e1f72861210aff3a1199be8a612814aba83
Thibault <tmarion90@gmail.com>
2018-03-09 12:59:55 +0100

    Read_header nearly complete, read_data working.

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ use std::fs::File;
 //use std::fs::OpenOptions;
 //use std::io;
 use std::io::prelude::*;
-//use std::io::SeekFrom;
+use std::io::SeekFrom;
 use std::path::{Path, PathBuf};
 use std::iter::*;
 //use std::fmt;
@@ -46,28 +46,10 @@ fn main() {
     }
 }
 
+
+
 struct TarArch {
     file: Option<File>,
-
-}
-
-impl TarArch {
-
-    fn new(path: PathBuf) -> Result<(TarArch)> {
-        Ok(TarArch {
-            file: Some(File::open(path)
-                .chain_err(|| "Could not open file")?),
-        })
-    }
-
-    fn next_element(&mut self) -> Result<Option<TarElement>> {
-        let file = match self.file {
-            Some(ref mut file) => file,
-            None => bail!("No file opened, aborting."),
-        };
-        let mut element = TarElement::read_header(file)?;
-        Ok(element)
-    }
 
 }
 
@@ -85,10 +67,44 @@ impl Iterator for TarArch {
     }
 }
 
+impl TarArch {
+
+    fn new(path: PathBuf) -> Result<(TarArch)> {
+        Ok(TarArch {
+            file: Some(File::open(path)
+                .chain_err(|| "Could not open file")?),
+        })
+    }
+
+    fn next_element(&mut self) -> Result<Option<TarElement>> {
+        let file = match self.file {
+            Some(ref mut file) => file,
+            None => bail!("No file opened, aborting."),
+        };
+        let mut element = match TarElement::read_header(file)? {
+            Some(el) => el,
+            None => return Ok(None),
+        };
+        element.read_data(file)?;
+        Ok(Some(element))
+    }
+
+}
+
+
+
+
+
+
 struct TarElement {
     name: String,
+    mode: usize,
     size: usize,
     magic: String,
+    user: String,
+    group: String,
+    prefix: String,
+    data: Vec<u8>,
 }
 
 impl TarElement {
@@ -100,9 +116,13 @@ impl TarElement {
 
         if n == 0 { return Ok(None) }
 
-        let mut element = TarElement {
+        let element = TarElement {
             name: match buf_to_string(&buffer[0..100])? {
                 Some(s) => s,
+                None => return Ok(None),
+            },
+            mode: match buf_to_num(&buffer[100..100+8])? {
+                Some(n) => n,
                 None => return Ok(None),
             },
             size: match buf_to_num(&buffer[124..124+12])? {
@@ -113,17 +133,55 @@ impl TarElement {
                 Some(s) => s,
                 None => return Ok(None),
             },
+            user: match buf_to_string(&buffer[265..265+32])? {
+                Some(s) => s,
+                None => return Ok(None),
+            },
+            group: match buf_to_string(&buffer[297..297+32])? {
+                Some(s) => s,
+                None => return Ok(None),
+            },
+            prefix: match buf_to_string(&buffer[345..345+155])? {
+                Some(s) => s,
+                None => return Ok(None),
+            },
+            data: Vec::new(),
         };
 
         if element.magic != String::from("ustar") {
+            println!("magic value returns None");
             return Ok(None)
         }
 
         Ok(Some(element))
     }
 
+
+    fn read_data(&mut self, f: &mut File) -> Result<()> {
+        f.take(self.size as u64).read_to_end(&mut self.data)
+            .chain_err(|| format!("Error reading file {}", self.name))?;
+        f.seek(SeekFrom::Current(offset(&self.size, 512)))
+            .chain_err(|| "Error seeking position")?;
+        Ok(())
+    }
+
 }
 
+
+
+
+fn offset(size: &usize, block: usize) -> i64 {
+    // Takes a data chunk size and a standard block size,
+    // and returns the offset necessary to jump to the next
+    // block.
+    let modulo = (size % block) as i64;
+
+    if modulo == 0 {
+        0
+    } else {
+        block as i64 - modulo
+    }
+}
 
 fn buf_to_string(buf: &[u8]) -> Result<Option<String>> {
     let vec: Vec<_> = buf.iter()
@@ -143,25 +201,26 @@ fn buf_to_string(buf: &[u8]) -> Result<Option<String>> {
     }
 }
 
-
 fn buf_to_num(buf: &[u8]) -> Result<Option<usize>> {
-    println!("buflen: {}\nbuf: |{:?}|",buf.len(),buf);
     let string = match buf_to_string(buf)? {
         Some(s) => s,
         None => return Ok(None),
     };
-    println!("|{}|",string);
     let num = usize::from_str_radix(string.as_str(), 8)
         .chain_err(|| "Error parsing string to usize")?;
     Ok(Some(num))
 }
+
+
+
+
 
 fn run() -> Result<()> {
     let archive = TarArch::new(PathBuf::from("/tmp/a.tar"))?;
 
     for el in archive {
         let el = el?;
-        println!("name: {:?}\nsize: {:?}",el.name, el.size);
+        println!("name: {:?}\nsize: {:?}\nmagic: {:?}",el.name, el.size, el.magic);
 //        ::std::process::exit(0);
     }
 
